f820db8cd6523dc98ca920b79b1ce512982e6550
nulLeeKH <i_am@nulleekh.com>
2019-07-23 23:19:17 +0900

    [REFACTOR] Change owner informaiton of license in source

--- sample/2019ulsan/challenge/challenge.ino
+++ sample/2019ulsan/challenge/challenge.ino
@@ -3,7 +3,7 @@ version : v1.0.0-alpha
 
 MIT License
 
-Copyright (c) 2019 nulLeeKH <i_am@nulleekh.com>
+Copyright (c) 2019 Kyung-ha Lee <i_am@nulleekh.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
